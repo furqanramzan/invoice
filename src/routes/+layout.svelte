62562caf342d6aec3b8223e475722f820cbd83c8
Muhammmad Furqan <furqanramzan271996@gmail.com>
2026-01-23 21:17:38 +0500

    add shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import './layout.css';
   import favicon from '$lib/assets/favicon.svg';
+  import { ModeWatcher } from 'mode-watcher';
+  import DarkMode from '$lib/components/dark-mode.svelte';
 
   let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<DarkMode />
 {@render children()}
