0d838a1ec30fead71619a908ba4df7998e04a814
Muhammmad Furqan <furqanramzan271996@gmail.com>
2026-01-26 10:05:07 +0500

    improve ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,19 +11,23 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
-<nav class="flex items-center justify-between bg-gray-100 p-4 dark:bg-gray-900">
-  <div class="flex items-center space-x-4">
-    <a href={resolve('/')} class="text-lg font-bold">Invoice App</a>
-  </div>
-  <div class="flex items-center space-x-4">
-    <a href={resolve('/invoice')} class="text-sm">Invoices</a>
-    <a href={resolve('/invoice/upsert')} class="text-sm">Create Invoice</a>
-    <form method="post" action="/logout" class="text-sm" use:enhance>
-      <button>Sign out</button>
-    </form>
-    <DarkMode />
+<nav class="flex bg-gray-100 p-4 dark:bg-gray-900">
+  <div class="mx-auto max-w-7xl min-w-7xl">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center space-x-4">
+        <a href={resolve('/')} class="text-lg font-bold">Invoice App</a>
+      </div>
+      <div class="flex items-center space-x-4">
+        <a href={resolve('/invoice')} class="text-sm">Invoices</a>
+        <a href={resolve('/invoice/upsert')} class="text-sm">Create Invoice</a>
+        <form method="post" action="/logout" class="text-sm" use:enhance>
+          <button>Sign out</button>
+        </form>
+        <DarkMode />
+      </div>
+    </div>
   </div>
 </nav>
-<div class="mx-auto my-5 max-w-7xl space-y-4">
+<div class="mx-auto my-5 max-w-7xl min-w-7xl space-y-4">
   {@render children()}
 </div>
